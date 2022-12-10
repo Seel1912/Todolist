f9d58f058e50ad2ba268f9165a5e751c27961e33
Shark <rynyaz@gmail.com>
2022-12-10 14:43:27 +0700

    third commit

--- register.js
+++ register.js
@@ -14,6 +14,6 @@ login.addEventListener("click", (e) => {
     } else {
         localStorage.setItem(username.value, json);
         alert("Register Succes");
-        window.location.href = "login.html";
+        window.location.href = "index.html";
     }
 });
